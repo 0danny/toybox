a8d555ccf5f3af845e4f668d3aa336303b542210
0danny <dpaolone03@gmail.com>
2026-06-19 18:50:13 +1000

    toybox: add barebones imgui shell

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,72 @@
-#include <print>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    std::println("Hello World!");
+	if (! glfwInit())
+		return 1;
 
-    return 0;
+	const char* glsl_version = "#version 130";
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "ToyBox", nullptr, nullptr);
+
+	if (! window)
+	{
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+
+	ImGuiIO& io = ImGui::GetIO();
+	(void)io;
+
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	while (! glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		// Show Demo
+		ImGui::ShowDemoWindow();
+        ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+
+		// Render
+		ImGui::Render();
+
+		int32_t width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+
+		glViewport(0, 0, width, height);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
 }
