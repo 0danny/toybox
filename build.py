03e1cbb48050f60ae2584ca965224adfeb873852
Mouks <36619434+mouksx@users.noreply.github.com>
2026-06-21 17:24:13 +1000

    removed msvc compilation

--- build.py
+++ build.py
@@ -18,7 +18,7 @@ def track_process(command, custom_name=""):
         text=True
     )
 
-    name = command[0] if custom_name == "" else custom_name
+    name = custom_name or command[0]
 
     for line in proc.stdout:
         print(f"[{name}]: {line.strip()}")
@@ -38,51 +38,31 @@ def get_executable_name():
     return f"{PROJECT_NAME}.exe" if os.name == "nt" else f"./{PROJECT_NAME}"
 
 
-def run_build(launch_app, build_type, use_msvc):
+def run_build(launch_app, build_type):
     print(f"[build]: Running build for {PROJECT_NAME} with type {build_type}")
 
     os.makedirs(BUILD_FOLDER, exist_ok=True)
     os.chdir(BUILD_FOLDER)
 
-    if use_msvc:
-        # Configure Visual Studio project
-        track_process([
-            "cmake",
-            "..",
-            "-G", "Visual Studio 18 2026",
-            "-A", "x64",
-            "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
-        ])
+    # Configure Ninja project
+    track_process([
+        "cmake",
+        "..",
+        "-G", "Ninja",
+        f"-DCMAKE_BUILD_TYPE={build_type}",
+        "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+    ])
 
-        # Build with MSBuild via CMake
-        track_process([
-            "cmake",
-            "--build",
-            ".",
-            "--config",
-            build_type
-        ])
-    else:
-        # Configure Ninja project
-        track_process([
-            "cmake",
-            "..",
-            "-G", "Ninja",
-            f"-DCMAKE_BUILD_TYPE={build_type}",
-            "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
-        ])
-
-        # Build with Ninja
-        track_process(["ninja"])
+    # Build with Ninja
+    track_process(["ninja"])
 
     copy_assets()
 
     if launch_app:
-        if not use_msvc:
-            exe_path = os.path.join(
-                os.getcwd(),
-                get_executable_name()
-            )
+        exe_path = os.path.join(
+            os.getcwd(),
+            get_executable_name()
+        )
 
         print(f"----------------- Starting {PROJECT_NAME} -----------------")
         subprocess.run([exe_path])
@@ -97,12 +77,6 @@ def main():
         "--nl",
         action="store_true",
         help="Don't launch the app after build"
-    )
-
-    parser.add_argument(
-        "--msvc",
-        action="store_true",
-        help="Use Visual Studio/MSVC instead of Ninja"
     )
 
     parser.add_argument(
@@ -121,8 +95,7 @@ def main():
 
     run_build(
         launch_app=not args.nl,
-        build_type=args.bt,
-        use_msvc=args.msvc
+        build_type=args.bt
     )
 
 
